6b93f02d8c12c89f17956faf833fa7b8e97032e9
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-12-12 11:13:57 -0800

    create function signinUser

---
+++ src/actions/index.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { browserHistory } from 'react-router';
+import { AUTH_USER } from './types';
+
+const ROOT_URL = 'http://localhost:3090';
+
+export function signinUser({ email, password }) {
+  return function(dispatch) {
+    // Submit email and password to server
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+      .then(response => {
+        // If request is good:
+        // -Update state to indicate user is authenticated
+        dispatch({ type: AUTH_USER });
+        // -Save JWT token
+
+        // -Redirect to route '/feature'
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
+        // If request is bad:
+        // -Show error to user
+        
+      });
+  }
+}
