cb11f921fd81d4908849399bc4a6e94c55c1bb53
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-12-15 20:54:41 -0800

    create welcome index route

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import App from './components/app';
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
 import Signup from './components/auth/signup';
+import Welcome from './components/welcome';
 import reducers from './reducers';
 import SampleFeature from './components/sample_feature';
 import RequireAuth from './components/auth/require_auth';
@@ -19,6 +20,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
+        <IndexRoute component={Welcome} />
         <Route path="signin" component={Signin} />
         <Route path="signout" component={Signout} />
         <Route path="signup" component={Signup} />
