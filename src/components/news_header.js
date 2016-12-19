c2ed61eaa2a4fb47fbae753dfb97cb72defe3737
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-12-19 13:36:14 -0800

    create NewsHeader

---
+++ src/components/news_header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NewsHeader = () => (
+  <div className="jumbotron">
+    <h1 className="display-3">News Aggregator</h1>
+    <p className="lead">Get the latest news from 60 sources.</p>
+  </div>
+);
+
+export default NewsHeader;
