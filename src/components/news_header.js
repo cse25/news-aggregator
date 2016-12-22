fbf1c3445e84c2299a649d19d770c88ddb0de908
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-12-21 18:44:39 -0800

    refactor using Jumbotron component

--- src/components/news_header.js
+++ src/components/news_header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
 
 const NewsHeader = () => (
-  <div className="jumbotron">
+  <Jumbotron>
     <h1 className="display-3">News Aggregator</h1>
     <p className="lead">Get the latest news from 60 sources.</p>
-  </div>
+  </Jumbotron>
 );
 
 export default NewsHeader;
