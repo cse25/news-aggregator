c46e09d20e860fa7a32d20a4d95a98775f06b807
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-12-21 18:44:24 -0800

    refactor to stateless component

--- src/components/news_dashboard.js
+++ src/components/news_dashboard.js
@@ -4,16 +4,12 @@ import ArticleList from './article_list';
 import NewsHeader from './news_header';
 import NewsSourceList from './news_source_list';
 
-class NewsDashboard extends Component {
-  render() {
-    return (
-      <div>
-        <NewsHeader />
-        <NewsSourceList />
-        <ArticleList />
-      </div>
-    )
-  }
-}
+const NewsDashboard = () => (
+  <div>
+    <NewsHeader />
+    <NewsSourceList />
+    <ArticleList />
+  </div>
+)
 
 export default NewsDashboard;
