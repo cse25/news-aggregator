f2cfdb40dd0b23f589c4da68c8b8839660c626b8
chrisjc25@gmail.com <chrisjc25@gmail.com>
2016-12-27 22:47:10 -0800

    refactor favorites list component

--- src/components/favorites.js
+++ src/components/favorites.js
@@ -1,24 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { ListGroup, ListGroupItem, Button } from 'react-bootstrap';
+import { ListGroup, ListGroupItem, Button, Glyphicon } from 'react-bootstrap';
 import { toggleFavorite } from '../actions/index';
 
 class Favorites extends Component {
   renderList() {
     return this.props.favorites.map((favorite) => {
-      return (
-        <ListGroupItem
-          key={favorite.name}
-          className="favorite-item">
-          {favorite.name}
-          <span><input 
-            className="checkbox" 
-            type="checkbox"
-            onClick={() => this.props.toggleFavorite(favorite)} />
-          </span>
-        </ListGroupItem>
-      )
+      if (!favorite.favorite) {
+        return (
+          <ListGroupItem
+            onClick={() => this.props.toggleFavorite(favorite)}
+            key={favorite.name}
+            className="favorite-item">
+            {favorite.name}
+          </ListGroupItem>
+        )
+      } else {
+        return (
+          <ListGroupItem
+            onClick={() => this.props.toggleFavorite(favorite)}
+            key={favorite.name}
+            className="favorite-item">
+            {favorite.name}
+            <span className="float-right">
+              <Glyphicon glyph="star" />
+            </span>
+          </ListGroupItem>
+        )
+      }
     })
   }
 
@@ -31,7 +41,7 @@ class Favorites extends Component {
         </ListGroup>
         <Button 
           bsStyle="primary"
-          className="save-button">
+          className="float-right">
           Save
         </Button>
       </div>
